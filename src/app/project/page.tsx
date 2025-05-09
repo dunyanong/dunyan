dceeaf1ca0bab381e141c8c981acc3e5443b139c
Ong Dun Yan <ongdunyan@Ongs-MacBook-Air.local>
2025-05-10 01:59:37 +0800

    rename the imported project to projectData

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,29 +1,29 @@
 import { Column } from "@/once-ui/components";
 import { baseURL } from "@/app/resources";
-import { about, person, project } from "@/app/resources/content";
+import { about, person, project as projectData } from "@/app/resources/content";
 import { Meta, Schema } from "@/once-ui/modules";
 import { Projects } from "@/components/project/Projects";
 
 export async function generateMetadata() {
   return Meta.generate({
-    title: project.title,
-    description: project.description,
+    title: projectData.title,
+    description: projectData.description,
     baseURL: baseURL,
-    image: `${baseURL}/og?title=${encodeURIComponent(project.title)}`,
-    path: project.path,
+    image: `${baseURL}/og?title=${encodeURIComponent(projectData.title)}`,
+    path: projectData.path,
   });
 }
 
-export default function project() {
+export default function ProjectPage() {
   return (
     <Column maxWidth="m">
       <Schema
         as="webPage"
         baseURL={baseURL}
-        path={project.path}
-        title={project.title}
-        description={project.description}
-        image={`${baseURL}/og?title=${encodeURIComponent(project.title)}`}
+        path={projectData.path}
+        title={projectData.title}
+        description={projectData.description}
+        image={`${baseURL}/og?title=${encodeURIComponent(projectData.title)}`}
         author={{
           name: person.name,
           url: `${baseURL}${about.path}`,
